f3b959d00172e437f6c38832ff31f8a15dbbc991
hessamoddin <hessamoddin.shafeian@gmail.com>
2016-04-10 17:45:29 -0400

    Update Read_Video_Imageio.py

--- Read_Video_Imageio.py
+++ Read_Video_Imageio.py
@@ -1,14 +1,14 @@
-# Author: Hessam
-# read a video frame, resize it and covert to rgb
 import pylab
 import imageio
 from skimage.transform import resize
 from skimage.color import rgb2gray
 
-filename="person15_walking_d1_uncomp.avi"
+filename="/home/hessam/Desktop/movies/Downton Abbey _ Starts Sunday 20 September-WPwaCjE8DcY.mkv"
 vid = imageio.get_reader(filename,  'ffmpeg')
+# number of frames in video
+num_frames=vid._meta['nframes']
 #10th frame
-frame = vid.get_data(10) 
+frame = vid.get_data(1) 
 #resize frame
 frame_resized=resize(frame, (100, 100))
 #convert the color frame to gray-scale
@@ -17,4 +17,3 @@ frame_gray= rgb2gray(frame_resized)
 fig = pylab.figure()
 fig.suptitle('image #{}'.format(10), fontsize=20)
 pylab.imshow(frame_gray)
-
