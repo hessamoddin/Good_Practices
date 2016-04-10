c931d1ab2759a6006e5d8499ad2f712696dc784b
hessamoddin <hessamoddin.shafeian@gmail.com>
2016-04-10 14:09:26 -0400

    Update Read_Video_Imageio.py

--- Read_Video_Imageio.py
+++ Read_Video_Imageio.py
@@ -1,9 +1,20 @@
+# Author: Hessam
+# read a video frame, resize it and covert to rgb
 import pylab
 import imageio
-filename="test_video.avi"
+from skimage.transform import resize
+from skimage.color import rgb2gray
+
+filename="person15_walking_d1_uncomp.avi"
 vid = imageio.get_reader(filename,  'ffmpeg')
-frame = vid.get_data(10) #10th frame
+#10th frame
+frame = vid.get_data(10) 
+#resize frame
+frame_resized=resize(frame, (100, 100))
+#convert the color frame to gray-scale
+frame_gray= rgb2gray(frame_resized)
+
 fig = pylab.figure()
-fig.suptitle('image #{}'.format(num), fontsize=20)
-pylab.imshow(frame)
-pylab.show()
+fig.suptitle('image #{}'.format(10), fontsize=20)
+pylab.imshow(frame_gray)
+
