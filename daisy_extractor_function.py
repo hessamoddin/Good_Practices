c1d422882e6438504b5cff67a28fb92d9af2dc38
hessamoddin <hessamoddin.shafeian@gmail.com>
2016-04-12 21:00:59 -0400

    Update daisy_extractor_function.py

--- daisy_extractor_function.py
+++ daisy_extractor_function.py
@@ -1,19 +1,3 @@
-# encoding: utf-8
-
-#  Copyright 2015-present Scikit Flow Authors. All Rights Reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-
 from __future__ import division, print_function, absolute_import
 from six.moves import cPickle
 import itertools
@@ -29,11 +13,6 @@ from skimage.feature import daisy
 import Daisy_Extractor
 
 def Daisy_Extractor_Fn(vid,frame_no,new_shape=(120,180),step=16, radius=10):
-#filename="v_shooting_16_03.avi"
-#filename=="v_biking_08_01.avi"
-#filename="v_biking_06_04.avi"
-#filename="v_swing_23_02.avi"
-#frame_no=111
     if frame_no<num_frames: 
         frame = vid.get_data(frame_no) 
         frame_resized=resize(frame, new_shape)
